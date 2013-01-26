97158d3eb75bd2b244f4580e51e0d45bcbd700fe
Nick Dreckshage <nicholasdreckshage@gmail.com>
2013-01-26 13:17:27 -0500

    improvement to paragraph to json tool. converts without editing html, through textbox on page. also trims trailing/leading whitespace.

--- tools/convert_to_json.js
+++ tools/convert_to_json.js
@@ -4,45 +4,58 @@
 
 $(function() {
 
-  var convert = $('.convert').html();
+  var convert = $('#convert'),
+      converter = $('#converter'),
+      converted = $('#converted');
 
-  function ellipses(convert,search,replace){
-    var position = convert.indexOf(search);
-      while (typeof position === 'number' && position > 0) {
-        convert = convert.replace(search, replace);
-        position = convert.indexOf(search);
-      }
-    return convert;
+  function convertToJSON(convertParagraph) {
+    // call function to take out ellipses so they don't conflict
+    convertParagraph = ellipses(convertParagraph,"...","<ELLIPSES>");
+
+    // split returned string on periods to create array
+    convertParagraph = convertParagraph.split('.');
+
+    // this gets rid of any blank indexes
+    convertParagraph = $.grep(convertParagraph,function(n){
+      return(n);
+    });
+
+    // TO DO, reference in jquery foreach? foreach ($x as &$y)...
+    var convertEach = new Array();
+
+    // put the ellipses back in and add a period
+    $.each(convertParagraph, function(key, value) {
+      value = ellipses(value,"<ELLIPSES>","...");
+      value = value + ".";
+      // kill leading/trailing spaces
+      value = value.replace(/(^\s+|\s+$)/g, '');
+      convertEach.push(value);
+    });
+
+    // seems silly, set convert paragraph back to convert each
+    convertParagraph = convertEach;
+
+    // convert it to JSON and return
+    return JSON.stringify(convertParagraph);
   }
 
-  // take out ellipses so they don't conflict
-  convert = ellipses(convert,"...","<ELLIPSES>");
+  function ellipses(convertParagraph,search,replace){
+    var position = convertParagraph.indexOf(search);
+    while (typeof position === 'number' && position > 0) {
+      convertParagraph = convertParagraph.replace(search, replace);
+      position = convertParagraph.indexOf(search);
+    }
+    return convertParagraph;
+  }
 
-  // split on periods
-  convert = convert.split('.');
-
-  // this gets rid of any blank indexes
-  convert = $.grep(convert,function(n){
-    return(n);
+  // when button is clicked
+  converter.on('click', function() {
+    // grab the value of the input
+    convertParagraph = convert.val();
+    // send it through the convert function
+    convertParagraph = convertToJSON(convertParagraph);
+    // and place it in the other input
+    converted.val(convertParagraph);
   });
-
-  // TO DO, reference in jquery foreach? foreach ($x as &$y)...
-  var convertEach = new Array();
-
-  // put the ellipses back in and add a period
-  $.each(convert, function(key, value) {
-    value = ellipses(value,"<ELLIPSES>","...");
-    value = value + ".";
-    convertEach.push(value);
-  });
-
-  // seems silly
-  convert = convertEach;
-
-  // convert it to JSON
-  convert = JSON.stringify(convert);
-
-  // replace the paragraph div with the JSON
-  $('.convert').text(convert);
 
 });
