1a1068897910ca282ca7e512b78a616f9afe5c62
Nick Dreckshage <nicholasdreckshage@gmail.com>
2013-02-09 18:29:33 -0500

    pages update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
 $(function(){
-  $(window).roughDraft({
-    'author' : 'baconipsum.com',
-    'illustrator' : 'placehold.it',
-    'thesaurus': 'assets/js/roughdraft.thesaurus.json'
-  });
+  $(window).roughDraft();
   prettyPrint();
 });
